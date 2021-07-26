3d2cd52b888e3df8e9644b116180a3f1421fcd84
Lcx-whitegive <1758811566@qq.com>
2021-07-26 12:57:24 +0800

    test git husky

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,16 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
+
+
+
+
   }
 })
 </script>
