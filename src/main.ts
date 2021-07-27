0be13439f462dec5c0e83182767902d317e08602
Lcx-whitegive <1758811566@qq.com>
2021-07-27 11:22:44 +0800

    feat: test commitlint

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// 全局引入element-plus
+// import ElementPlus from 'element-plus'
+// import 'element-plus/lib/theme-chalk/index.css'
+
 import router from '@/router/index'
 
 import store from '@/store/index'
